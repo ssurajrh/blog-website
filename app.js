03aa3e32f9b50106829ec0196b419914a51327dd
suraj h <surajj.hiremath@gmail.com>
2023-03-24 22:11:54 +0530

    home page added with header and footer

--- app.js
+++ app.js
@@ -11,12 +11,13 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const app = express();
 
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-
+app.get("/",function(req,res){
+  res.render("home",{})
+})
 
 
 
